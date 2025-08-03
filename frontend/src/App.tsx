1bb324822238886b9f85690933125a3968021761
Netnaphat <Netnaphat.cha@gmail.com>
2025-08-04 02:41:24 +0700

    Remove frontend as submodule and add as normal folder

---
+++ frontend/src/App.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home/Home"
+import JobPost from "./pages/JobPost/JobPost";
+import Board from './pages/Board/Board';
+import JobDetail from "./pages/Board/JobDetail";
+
+
+const App: React.FC = () => {
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/board" element={<Board />} />
+        <Route path="/post-job" element={<JobPost />} />
+<Route path="/post-detail" element={<JobDetail />} />
+
+      </Routes>
+    </>
+  );
+};
+
+export default App;
